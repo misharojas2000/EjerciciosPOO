e7090a82b1071f188dd91047382e140e2ca91922
Fernanda Mishell Monzón Rojas <mishellrojas2016@gmail.com>
2025-08-02 16:31:34 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio020825.2/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio020825._2
+{
+    internal class Empleado
+    {
+        public string Nombre { get; set; };
+        public double Salario { get; set; };
+
+    }
+
+    internal class Gerente : Empleado {
+        public void TomarDecision();
+        Console.WriteLine("Tomar una desicion")
+     }
+
+    internal class Desarrollador : Empleado
+    {
+        public void Programar();
+        Console.WriteLine("Programar una aplicacion");
+    }
+
+
+    public  class Vehiculo { }
+    {
+        public Avanzar()
+}
+
+}
---
+++ Ejercicio020825/Empleado.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio020825
+{
+    public class Empleado
+    {
+        private string nombre;
+        private double salario;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { if (string.IsNullOrWhiteSpace(value)){
+                    nombre = value;
+                }
+            } }
+        public double Salario
+        {
+            get { return salario; }
+            set
+            {
+                if(value >= 0)
+                {
+                    salario= value;
+                }
+            }
+        }
+    }
+
+    public class CuentaBancaria
+    {
+        private double saldo;
+        public int limite = 1000;
+
+        public CuentaBancaria(double Saldo)
+        {
+            saldo = Saldo;
+        }
+
+        public void Retirar(double monto)
+        {
+            if (monto <= saldo)
+            {
+                saldo -= monto;
+            } else if (monto > limite)
+            {
+                Console.WriteLine($"No es posible retirar Q{monto}, ya que el limite de retiro diario es de {limite}. ");
+            }
+        }
+
+        public double MostrarSaldo()
+        {
+            return saldo;
+        }
+    }
+
+    public class Vehiculo
+    {
+        public bool encendido = true;
+        public bool apagado = false;
+    }
+
+}
